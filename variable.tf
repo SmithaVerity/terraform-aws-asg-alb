d07aff2469d0abfd1620758ab84dcd56bedff665
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-04-01 22:29:21 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,9 @@
 variable "desired_capacity" {
-    default    = 3
+    default    = 2
 }
-variable "max_size" {}
+variable "max_size" {{
+    default    = 5
+}
 
 variable "min_size" {
     default    = 1
