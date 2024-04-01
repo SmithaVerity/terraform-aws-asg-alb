ac9a441f948d57fd8a25c1acb8a6154dee02d60e
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-04-01 23:39:26 +0530

    Update lt.tf

--- lt.tf
+++ lt.tf
@@ -2,7 +2,6 @@ resource "aws_launch_template" "project" {
   name_prefix          = "project"
   image_id             = var.image_id
   instance_type        = var.instance_type
-  key_name             = aws_key_pair.asg_key_pair.key_name
   tags                 = var.tags
   
  
