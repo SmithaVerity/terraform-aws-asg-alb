08be7857f47e211e70217282f1d97af3f2f3a80c
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-04-02 01:46:34 +0530

    Update lt.tf

--- lt.tf
+++ lt.tf
@@ -4,5 +4,12 @@ resource "aws_launch_template" "project" {
   instance_type        = var.instance_type
   tags                 = var.tags
   vpc_security_group_ids = [aws_security_group.alb_sec_group.id]
- 
+  
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "ctpscalabilitytest"
+    }
+  }
 }
